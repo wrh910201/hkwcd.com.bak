e63a73b4d59616df22e1d97986cde8b0e0a3876a
wrh <wrh4285@163.com>
2017-02-23 22:26:02 +0800

    个人中心添加发货完成

--- App/Group/Index/Action/DeliveryAction.class.php
+++ App/Group/Index/Action/DeliveryAction.class.php
@@ -48,39 +48,70 @@ class DeliveryAction extends BaseAction {
         if( !IS_POST ) {
             exit;
         }
+        $data['company'] = I('post.company', '', 'trim');
         $data['consignor'] = I('post.consignor', '', 'trim');
         $data['country_id'] = I('post.country', 0, 'intval');
         $data['state'] = I('post.state', '', 'trim');
         $data['city'] = I('post.city', '', 'trim');
-        $data['detail_address'] = I('post.address', '', 'trim');
+        $data['detail_address'] = I('post.detail_address', '', 'trim');
         $data['mobile'] = I('post.mobile', '', 'trim');
         $data['phone'] = I('post.phone', '', 'trim');
         $data['postal_code'] = I('post.postal_code', '', 'trim');
-        $data['is_default'] = I('post.default', 0, 'intval');
+        $data['certificate_num'] = I('post.certificate_num', '', 'trim');
+        $data['certificate1_url'] = I('post.certificate1_url', '', 'trim');
+        $data['certificate2_url'] = I('post.certificate2_url', '', 'trim');
+        $data['is_default'] = I('post.is_default', 0, 'intval');
 
         if( empty($data['consignor']) ) {
-            $this->error('请输入发货人');
+            $this->error('请输入联系人');
         }
         if( $data['country_id'] <= 0 ) {
             $this->error('请选择国家');
         }
+        if( empty($data['city']) ) {
+            $this->error('请输入城市');
+        }
         if( empty($data['detail_address']) ) {
             $this->error('请输入具体地址');
         }
-        if( empty($data['mobile']) && empty($data['phone']) ) {
-            $this->error('手机与座机至少输入一个');
+        if( empty($data['postal_code']) ) {
+            $this->error('请输入邮编');
         }
+        if( empty($data['mobile']) ) {
+            $this->error('请输入手机');
+        }
+
+        if( empty($data['certificate_num'])  ) {
+            $this->error('请输入身份证号码');
+        }
+
+        if( empty($data['certificate1_url'])  ) {
+            $this->error('请上传身份证正面');
+        }
+
+        if( empty($data['certificate2_url'])  ) {
+            $this->error('请上传身份证反面');
+        }
+
         $data['is_default'] = $data['is_default'] == 1 ? 1:0;
 
         $data['client_id'] = session('hkwcd_user.user_id');
 
         if( $data['is_default'] ) {
-            $old_where = ['is_default' => 1, 'status' => 1, 'client_id' => $data['client_id']];
+            $old_where = ['is_default' => 1, 'client_id' => $data['client_id']];
             M('DeliveryAddress')->where($old_where)->save(['is_default' => 0]);
         }
         $result = M('DeliveryAddress')->add($data);
         if( $result ) {
-            $this->success('添加发货地址成功', U('Delivery/index'));
+            //加水印
+            $data['id'] = M('DeliveryAddress')->getLastInsID();
+            $result1 = $this->_certificate_water($data);
+
+            if( $result1 ) {
+                $this->success('添加发货地址成功', U('Delivery/index'));
+            } else {
+                $this->error('系统繁忙，请稍后重试');
+            }
         } else {
             $this->error('系统繁忙，请稍后重试');
         }
@@ -97,6 +128,7 @@ class DeliveryAction extends BaseAction {
         $has_error = false;
         $errorMsg = '';
 
+        $data['company'] = I('post.company', '', 'trim');
         $data['consignor'] = I('post.consignor', '', 'trim');
         $data['country_id'] = I('post.country', 0, 'intval');
         $data['state'] = I('post.state', '', 'trim');
@@ -105,28 +137,51 @@ class DeliveryAction extends BaseAction {
         $data['mobile'] = I('post.mobile', '', 'trim');
         $data['phone'] = I('post.phone', '', 'trim');
         $data['postal_code'] = I('post.postal_code', '', 'trim');
+        $data['certificate_num'] = I('post.certificate_num', '', 'trim');
+        $data['certificate1_url'] = I('post.certificate1_url', '', 'trim');
+        $data['certificate2_url'] = I('post.certificate2_url', '', 'trim');
         $data['is_default'] = I('post.is_default', 0, 'intval');
 
         if( empty($data['consignor']) ) {
             $has_error = true;
-            $errorMsg .= empty($errorMsg) ? '请输入发货人' : '<br />请输入发货人';
+            $errorMsg .= empty($errorMsg) ? '请输入联系人' : '<br />请输入联系人';
         }
         if( $data['country_id'] <= 0 ) {
             $has_error = true;
             $errorMsg .= empty($errorMsg) ? '请选择国家' : '<br />请选择国家';
         }
+        if( empty($data['city']) ) {
+            $has_error = true;
+            $errorMsg .= empty($errorMsg) ? '请输入城市' : '<br />请输入城市';
+        }
         if( empty($data['detail_address']) ) {
             $has_error = true;
             $errorMsg .= empty($errorMsg) ? '请输入具体地址' : '<br />请输入具体地址';
-        }
-        if( empty($data['mobile']) && empty($data['phone']) ) {
-            $has_error = true;
-            $errorMsg .= empty($errorMsg) ? '手机与座机至少输入一个' : '<br />手机与座机至少输入一个';
         }
 
         if( empty($data['postal_code']) ) {
             $has_error = true;
             $errorMsg .= empty($errorMsg) ? '请输入邮编' : '<br />请输入邮编';
+        }
+
+        if( empty($data['mobile'])  ) {
+            $has_error = true;
+            $errorMsg .= empty($errorMsg) ? '请输入手机' : '<br />请输入手机';
+        }
+
+        if( empty($data['certificate_num'])  ) {
+            $has_error = true;
+            $errorMsg .= empty($errorMsg) ? '请输入身份证号码' : '<br />请输入身份证号码';
+        }
+
+        if( empty($data['certificate1_url'])  ) {
+            $has_error = true;
+            $errorMsg .= empty($errorMsg) ? '请上传身份证正面' : '<br />请上传身份证正面';
+        }
+
+        if( empty($data['certificate2_url'])  ) {
+            $has_error = true;
+            $errorMsg .= empty($errorMsg) ? '请上传身份证反面' : '<br />请上传身份证反面';
         }
 
         if( $has_error ) {
@@ -140,15 +195,23 @@ class DeliveryAction extends BaseAction {
         $data['client_id'] = session('hkwcd_user.user_id');
 
         if( $data['is_default'] ) {
-            $old_where = ['is_default' => 1, 'status' => 1, 'client_id' => $data['client_id']];
+            $old_where = ['is_default' => 1, 'client_id' => $data['client_id']];
             M('DeliveryAddress')->where($old_where)->save(['is_default' => 0]);
         }
 
         $result = M('DeliveryAddress')->add($data);
         if( $result ) {
-            $response['code'] = 1;
-            $data['id'] = $result;
-            $response['data'] = $data;
+            //加水印
+            $data['id'] = M('DeliveryAddress')->getLastInsID();
+            $result1 = $this->_certificate_water($data);
+
+            if( $result1 ) {
+                $response['code'] = 1;
+                $data['id'] = $result;
+                $response['data'] = $data;
+            } else {
+                $response['msg'] = '系统繁忙，请稍后重试';
+            }
         } else {
             $response['msg'] = '系统繁忙，请稍后重试';
         }
@@ -261,5 +324,61 @@ class DeliveryAction extends BaseAction {
         } else {
             $this->error('系统繁忙，请稍后重试');
         }
+    }
+
+    private function _certificate_water($data) {
+        $update_data = [];
+        $delivery_id = $data['id'];
+        $username = session('hkwcd_user.username');
+        if( !file_exists(THINK_PATH.'../uploads/client/'.$username.'/') ) {
+            mkdir(THINK_PATH.'../uploads/client/'.$username.'/');
+        }
+        if( !file_exists(THINK_PATH.'../uploads/client/'.$username.'/delivery') ) {
+            mkdir(THINK_PATH.'../uploads/client/'.$username.'/delivery');
+        }
+        //正面
+        $url = realpath(THINK_PATH.'../'.$data['certificate1_url']);
+        $ext_array = explode('.', $data['certificate1_url']);
+        $ext1 = $ext_array[1];
+        $save_name_1 = THINK_PATH.'../uploads/client/'.$username.'/delivery/'.$delivery_id.'_front.'.$ext1;
+        $update_save_name_1 = '/uploads/client/'.$username.'/delivery/'.$delivery_id.'_front.'.$ext1;
+        $result = $this->_add_water($url, $save_name_1);
+        if( $result ) {
+            $update_data['certificate1_url'] = $update_save_name_1;
+        }
+        @unlink($url);
+
+        //反面
+        $url = realpath(THINK_PATH.'../'.$data['certificate2_url']);
+        $ext_array = explode('.', $data['certificate2_url']);
+        $ext2 = $ext_array[1];
+        $save_name_2 = THINK_PATH.'../uploads/client/'.$username.'/delivery/'.$delivery_id.'_back.'.$ext1;
+        $update_save_name_2 = '/uploads/client/'.$username.'/delivery/'.$delivery_id.'_back.'.$ext2;
+        $result = $this->_add_water($url, $save_name_2);
+        if( $result ) {
+            $update_data['certificate2_url'] = $update_save_name_2;
+        }
+        @unlink($url);
+        $map = [
+            'id' => $delivery_id,
+        ];
+        if( file_exists($save_name_1) && file_exists($save_name_2) ) {
+//            $update_data['status'] = 1;
+            M('DeliveryAddress')->where($map)->save($update_data);
+            return true;
+        } else {
+            M('DeliveryAddress')->where($map)->delete();
+            return false;
+        }
+    }
+
+
+    private function _add_water($url, $savename) {
+        import('ORG.Util.Image.ThinkImage');
+        $shuiyin = realpath(THINK_PATH.'../Public/config/images/shuiyin.png');
+        $image = new ThinkImage(THINKIMAGE_GD, $url);
+        $result = $image->water($shuiyin);
+        $result->save($savename);
+        return true;
     }
 }
