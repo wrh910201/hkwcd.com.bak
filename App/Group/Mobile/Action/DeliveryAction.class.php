1d72ec22d1f11c00d6b79653b45922e7abc79c35
wrh910201 <dudu910316>
2017-10-13 11:37:40 +0800

    手机版 发货列表

--- App/Group/Mobile/Action/DeliveryAction.class.php
+++ App/Group/Mobile/Action/DeliveryAction.class.php
@@ -9,6 +9,25 @@
 class DeliveryAction extends Action {
 
     public function index() {
+
+        $where['client_id'] = session('hkwcd_user.user_id');
+        $where['status'] = 1;
+        // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        //分页
+        import('ORG.Util.Page');
+        $count = M('DeliveryAddress')->where($where)->count();
+
+        $page = new Page($count, C('usercenter_page_count'));
+        $limit = $page->firstRow. ',' .$page->listRows;
+
+        $delivery_list =M('DeliveryAddress')->where($where)->limit($limit)->order('is_default desc, id asc')->select();
+        if( $delivery_list ) {
+            foreach( $delivery_list  as $k => $v ) {
+                $delivery_list[$k]['index'] = $k+1;
+            }
+        }
+        $this->assign('delivery_list',$delivery_list);// 赋值数据集
+
         $this->display();
 
     }
