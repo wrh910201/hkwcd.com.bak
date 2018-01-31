7c596e765466bcbb8b4c0b232166dd984f929cc6
wrh910201 <dudu910316>
2018-01-31 17:34:30 +0800

    搜索国家以首字母

--- Public/wcd56/usercenter/js/base.js
+++ Public/wcd56/usercenter/js/base.js
@@ -29,3 +29,28 @@ function hide_loading() {
     layer.close(loading);
 }
 
+function matchStart(params, data) {
+    // If there are no search terms, return all of the data
+    if ($.trim(params.term) === '') {
+        return data;
+    }
+
+    // Skip if there is no 'children' property
+    if (typeof data.text === 'undefined') {
+        return null;
+    }
+
+    // `data.children` contains the actual options that we are matching against
+    if (data.text.toUpperCase().indexOf(params.term.toUpperCase()) == 0 ) {
+        var modifiedData = $.extend({}, data, true);
+        // modifiedData.text += ' (matched)';
+
+        // You can return modified objects from here
+        // This includes matching the `children` how you want in nested data sets
+        return modifiedData;
+    }
+
+    // Return `null` if the term should not be displayed
+    return null;
+}
+
