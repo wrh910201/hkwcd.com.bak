0eb2a9ae9f99bdd91d4f55883ae696280a8508b8
ezreal <wrh4285@163.com>
2018-07-19 18:33:41 +0800

    撤销上个版本的修改

--- App/Group/Manage/Action/ClientorderAction.class.php
+++ App/Group/Manage/Action/ClientorderAction.class.php
@@ -1434,9 +1434,9 @@ class ClientorderAction extends CommonContentAction {
         if( $transaction ) {
             foreach( $order_specifications as $k => $v ) {
                 foreach( $v['cargo'] as $d ) {
-                    if( empty($order_detail[$d['product_index']]['id']) ) {
-                        continue;
-                    }
+//                    if( empty($order_detail[$d['product_index']]['id']) ) {
+//                        continue;
+//                    }
                     $temp = [
                         'specifications_id' => $v['id'],
                         'detail_id' => $order_detail[$d['product_index']]['id'],
